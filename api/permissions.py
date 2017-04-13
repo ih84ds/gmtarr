c0204d86ca401f7c983a2ed688cdb942e3861734
Jeremy Parrish <jeremyp@rrish.org>
2017-04-13 01:57:41 -0500

    Adds a couple of permissions classes that allow admins to update/delete things while allowing others to only read.

---
+++ api/permissions.py
@@ -0,0 +1,24 @@
+from rest_framework.permissions import BasePermission, SAFE_METHODS
+from rest_framework.compat import is_authenticated
+
+class IsAdminUserOrReadOnly(BasePermission):
+    """
+    The request is authenticated as an admin user, or is a read-only request.
+    """
+
+    def has_permission(self, request, view):
+        return (
+            ( request.method in SAFE_METHODS ) or
+            ( request.user and is_authenticated(request.user) and request.user.is_staff )
+        )
+
+class IsAdminUserOrReadOnlyAuthenticated(BasePermission):
+    """
+    The request is authenticated as an admin user, or is a read-only request.
+    """
+
+    def has_permission(self, request, view):
+        return request.user and is_authenticated(request.user) and (
+            ( request.method in SAFE_METHODS ) or
+            ( request.user.is_staff )
+        )
