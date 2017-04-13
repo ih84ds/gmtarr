1d3e65b070210953deefd69c7ef4b3634e292f9d
Jeremy Parrish <jeremyp@rrish.org>
2017-04-13 13:44:28 -0500

    Adds the heart and soul of this entire app: The Round Robin scheduler.

--- api/utils.py
+++ api/utils.py
@@ -2,6 +2,35 @@ import datetime, json, re
 from waauth import utils as wautils
 from api.models import *
 
+def create_balanced_round_robin(list):
+    """ Create a schedule for the teams in the list and return it"""
+    s = []
+    if len(list) % 2 == 1: list = list + ["BYE"]
+    # manipulate map (array of indexes for list) instead of list itself
+    # this takes advantage of even/odd indexes to determine home vs. away
+    map = range(len(list))
+    mid = len(map) / 2
+    for i in range(len(map)-1):
+        l1 = map[:mid]
+        l2 = map[mid:]
+        l2.reverse()
+        round = []
+        for match in zip(l1, l2):
+            # team 1 is team with lower index
+            t1 = list[min(match)]
+            # team 2 is team with higher index
+            t2 = list[max(match)]
+            # this will equalize home/away for each team +/- 1
+            swap = ((match[0] + match[1]) % 2) == 1
+            if swap:
+                round.append((t2, t1))
+            else:
+                round.append((t1, t2))
+        s.append(round)
+        # rotate list, except for first element
+        map.insert(1, map.pop())
+    return s
+
 def format_phone(phone, format='{}-{}-{}'):
     nanp = re.compile('([2-9][0-9]{2})([^0-9]*)([2-9][0-9]{2})([^0-9]*)([0-9]{4})')
     m = nanp.match(phone)
