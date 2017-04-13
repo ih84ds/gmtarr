5812f4420190e7b2180262d74995d4033e25b4c9
Jeremy Parrish <jeremyp@rrish.org>
2017-04-13 09:43:16 -0500

    Fixes wa_user rcontext processor

--- waauth/context_processors.py
+++ waauth/context_processors.py
@@ -5,9 +5,7 @@ def waauth(request):
     wa_user = None
     try:
         if request.user.is_authenticated():
-            # wa_user = WAUser.objects.get(user=request.user)
-            # FIXME is this right?
-            wa_user = request.user
+            wa_user = WAUser.objects.get(user=request.user)
     except WAUser.DoesNotExist:
         pass
     return { 'wa_user': wa_user }
