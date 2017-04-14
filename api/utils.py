44eb77338a0994ebc8e58979f84578e9ffdf197d
Jeremy Parrish <jeremyp@rrish.org>
2017-04-13 23:30:58 -0500

    Better RR alg. Previous alg was balance, but gave players long streaks of home or away. This one has players alternating every round for the most part.

--- api/utils.py
+++ api/utils.py
@@ -2,33 +2,32 @@ import datetime, json, re
 from waauth import utils as wautils
 from api.models import *
 
-def create_balanced_round_robin(list):
-    """ Create a schedule for the teams in the list and return it"""
+def create_balanced_round_robin(players):
+    """ Create a schedule for the players in the list and return it"""
     s = []
-    if len(list) % 2 == 1: list = list + ["BYE"]
+    if len(players) % 2 == 1: players = players + ["BYE"]
     # manipulate map (array of indexes for list) instead of list itself
     # this takes advantage of even/odd indexes to determine home vs. away
-    map = range(len(list))
-    mid = len(map) / 2
-    for i in range(len(map)-1):
+    n = len(players)
+    map = list(range(n))
+    mid = n // 2
+    for i in range(n-1):
         l1 = map[:mid]
         l2 = map[mid:]
         l2.reverse()
         round = []
-        for match in zip(l1, l2):
-            # team 1 is team with lower index
-            t1 = list[min(match)]
-            # team 2 is team with higher index
-            t2 = list[max(match)]
-            # this will equalize home/away for each team +/- 1
-            swap = ((match[0] + match[1]) % 2) == 1
-            if swap:
+        for j in range(mid):
+            t1 = players[l1[j]]
+            t2 = players[l2[j]]
+            if j == 0 and i % 2 == 1:
+                # flip the first match only, every other round
+                # (this is because the first match always involves the last player in the list)
                 round.append((t2, t1))
             else:
                 round.append((t1, t2))
         s.append(round)
-        # rotate list, except for first element
-        map.insert(1, map.pop())
+        # rotate list by n/2, leaving last element at the end
+        map = map[mid:-1] + map[:mid] + map[-1:]
     return s
 
 def format_phone(phone, format='{}-{}-{}'):
