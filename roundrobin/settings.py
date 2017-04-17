20fa393fa05c011d528d922ffc5c1d2863b9903f
Jeremy Parrish <jeremyp@rrish.org>
2017-04-16 19:56:50 -0500

    Adds commented out nuclear options for CORS so it’s easy to remember its name if needed.

--- roundrobin/settings.py
+++ roundrobin/settings.py
@@ -153,6 +153,7 @@ JWT_AUTH = {
 # Set CSRF and CORS hosts in settings_local
 # CSRF_TRUSTED_ORIGINS = ( , )
 # CORS_ORIGIN_WHITELIST = ( , )
+# CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOW_METHODS = (
     'DELETE',
     'GET',
