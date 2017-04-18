0cfb05d5227731a7d416853d44dab4470242e98e
Jeremy Parrish <jeremyp@rrish.org>
2017-04-18 11:00:48 -0500

    Makes api/docs require login automatically

--- api/urls.py
+++ api/urls.py
@@ -1,13 +1,43 @@
 from django.conf.urls import url
+from django.contrib.auth.decorators import login_required
+from django.core.urlresolvers import RegexURLPattern, RegexURLResolver
 from rest_framework.documentation import include_docs_urls
 from rest_framework_jwt.views import refresh_jwt_token
 
 from . import views
 
+class DecoratedURLPattern(RegexURLPattern):
+    def resolve(self, *args, **kwargs):
+        result = super(DecoratedURLPattern, self).resolve(*args, **kwargs)
+        if result:
+            result.func = self._decorate_with(result.func)
+        return result
+
+class DecoratedRegexURLResolver(RegexURLResolver):
+    def resolve(self, *args, **kwargs):
+        result = super(DecoratedRegexURLResolver, self).resolve(*args, **kwargs)
+        if result:
+            result.func = self._decorate_with(result.func)
+        return result
+
+def decorated_includes(func, includes, *args, **kwargs):
+    urlconf_module, app_name, namespace = includes
+
+    for item in urlconf_module:
+        if isinstance(item, RegexURLPattern):
+            item.__class__ = DecoratedURLPattern
+            item._decorate_with = func
+
+        elif isinstance(item, RegexURLResolver):
+            item.__class__ = DecoratedRegexURLResolver
+            item._decorate_with = func
+
+    return urlconf_module, app_name, namespace
+
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^welcome$', views.welcome, name='welcome'),
-    url(r'^docs/', include_docs_urls(title='Round Robin API Documentation')),
+    url(r'^docs/', decorated_includes(login_required, include_docs_urls(title='Round Robin API Documentation'))),
 
     # Token Routes
     url(r'^auth-token$', views.auth_token, name='auth_token'),
