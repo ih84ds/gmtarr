189aaf2ff16e3ea74cf0467c6b6b5a75591c70ac
Jeremy Parrish <jeremyp@rrish.org>
2017-04-21 22:35:28 -0500

    Adds filtering and searching to admin backend

--- api/admin.py
+++ api/admin.py
@@ -3,17 +3,21 @@ from api.models import *
 
 @admin.register(League)
 class LeagueAdmin(admin.ModelAdmin):
-    pass
+    list_filter = ('year', 'event_id')
+    search_fields = ('name', )
     
 @admin.register(Flight)
 class FlightAdmin(admin.ModelAdmin):
-    pass
+    list_filter = ('year', 'league', 'ntrp', 'start_date')
+    search_fields = ('name', )
     
 @admin.register(Player)
 class PlayerAdmin(admin.ModelAdmin):
-    pass
+    list_filter = ('gender', 'ntrp', 'league', 'flight')
+    search_fields = ('name', 'email')
     
 @admin.register(Match)
 class MatchAdmin(admin.ModelAdmin):
-    pass
+    list_filter = ('year', 'flight__league', 'flight', 'scheduled_date', 'status')
+    search_fields = ('home_player__name', 'visitor_player__name')
     
